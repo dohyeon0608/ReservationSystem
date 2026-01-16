c693c283d033c8d89d52a5ef1468f532e7af52d3
dohyeon0608 <ywkim0608@gmail.com>
2026-01-16 23:17:29 +0900

    init: 프로젝트 생성

---
+++ src/test/java/io/github/dohyeon0608/web/reservation/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dohyeon0608.web.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1cff81a6089176d0e2c9d4eb5aa0a7ebf85728cc
dohyeon0608 <ywkim0608@gmail.com>
2026-02-21 22:08:42 +0900

    feat: 동시성 테스트 코드 추가

--- src/test/java/io/github/dohyeon0608/web/reservation/ReservationApplicationTests.java
+++ src/test/java/io/github/dohyeon0608/web/reservation/ReservationApplicationTests.java
@@ -4,23 +4,35 @@ import io.github.dohyeon0608.web.reservation.dto.request.PlaceRequestDto;
 import io.github.dohyeon0608.web.reservation.dto.request.ReservationRequestDto;
 import io.github.dohyeon0608.web.reservation.dto.request.TimeslotRequestDto;
 import io.github.dohyeon0608.web.reservation.dto.request.UserRegistrationDto;
+import io.github.dohyeon0608.web.reservation.entity.User;
 import io.github.dohyeon0608.web.reservation.entity.enums.OperationStatus;
 import io.github.dohyeon0608.web.reservation.entity.enums.PlaceType;
+import io.github.dohyeon0608.web.reservation.entity.enums.UserRole;
+import io.github.dohyeon0608.web.reservation.entity.mapping.Timeslot;
 import io.github.dohyeon0608.web.reservation.exception.BusinessException;
+import io.github.dohyeon0608.web.reservation.repository.PlaceRepository;
+import io.github.dohyeon0608.web.reservation.repository.ReservationRepository;
+import io.github.dohyeon0608.web.reservation.repository.TimeslotRepository;
+import io.github.dohyeon0608.web.reservation.repository.UserRepository;
 import io.github.dohyeon0608.web.reservation.service.PlaceService;
 import io.github.dohyeon0608.web.reservation.service.ReservationService;
 import io.github.dohyeon0608.web.reservation.service.TimeslotService;
 import io.github.dohyeon0608.web.reservation.service.UserService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
-@Transactional
 class ReservationApplicationTests {
 
 	@Autowired
@@ -35,12 +47,26 @@ class ReservationApplicationTests {
 	@Autowired
 	private ReservationService reservationService;
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PlaceRepository placeRepository;
+
+	@Autowired
+	private TimeslotRepository timeslotRepository;
+
+	@Autowired
+	private ReservationRepository reservationRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	public void 타임슬롯_시간_겹침방지() {
+	@Transactional
+	@DisplayName("타임슬롯 시간 겹침 방지")
+	public void preventTimeslotTimeDuplicated() {
 		LocalDateTime localDateTime = LocalDateTime.of(2026, 1, 20, 13, 0, 0);
 
 		PlaceRequestDto placeDto = PlaceRequestDto.builder()
@@ -88,7 +114,9 @@ class ReservationApplicationTests {
 	}
 
 	@Test
-	public void 중복_예약_방지_테스트() {
+	@Transactional
+	@DisplayName("중복 예약 방지")
+	public void preventDuplicatedReservations() {
 		LocalDateTime localDateTime = LocalDateTime.of(2026, 1, 20, 13, 0, 0);
 
 		UserRegistrationDto registerationDto1 = UserRegistrationDto.builder()
@@ -163,6 +191,85 @@ class ReservationApplicationTests {
 		});
 
 
+	}
+
+	@Test
+	@DisplayName("예약 동시성 방지")
+	public void testReservationSynchronicity() throws InterruptedException {
+		int threadCount = 5;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		// 데이터 세팅
+		List<Long> userIdList = new ArrayList<>();
+
+		for(int i = 0; i < threadCount; i++) {
+			UserRegistrationDto userRegistrationDto = UserRegistrationDto.builder()
+					.email(String.format("user%d@example.com", i))
+					.password("1234")
+					.nickname(String.format("테스트 사용자 %d", i))
+					.role(UserRole.USER)
+					.build();
+
+			userIdList.add(userService.createUser(userRegistrationDto));
+		}
+
+		PlaceRequestDto placeDto = PlaceRequestDto.builder()
+				.placeType(PlaceType.STUDY_ROOM)
+				.operationStatus(OperationStatus.OPENED)
+				.name("의혈스터디룸")
+				.build();
+
+		Long placeId = placeService.createPlace(placeDto);
+
+		LocalDateTime localDateTime = LocalDateTime.of(2026, 1, 20, 13, 0, 0);
+
+		TimeslotRequestDto timeslotRequestDto = TimeslotRequestDto.builder()
+				.placeId(placeId)
+				.reservationDate(localDateTime.toLocalDate())
+				.startTime(localDateTime.toLocalTime())
+				.endTime(localDateTime.toLocalTime().plusHours(2))
+				.maxCapacity(1)
+				.build();
+
+		Long timeslotId = timeslotService.createTimeslot(timeslotRequestDto);
+
+		ReservationRequestDto reservationDto = ReservationRequestDto.builder()
+				.timeslotId(timeslotId)
+				.build();
+
+		List<Long> reservationIdList = new ArrayList<>();
+
+		for (int i = 0; i < threadCount; i++) {
+			Long userId = userIdList.get(i);
+			executorService.execute(() -> {
+				try {
+					reservationIdList.add(reservationService.createReservation(userId, reservationDto));
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+
+		Timeslot timeslot = timeslotService.getTimeslotById(timeslotId);
+
+		Assertions.assertSame(1, timeslot.getReservationCount());
+
+		// 삭제
+		for(Long reservationId : reservationIdList) {
+			reservationRepository.delete(reservationService.getReservationById(reservationId));
+		}
+
+		timeslotRepository.delete(timeslot);
+
+		placeRepository.delete(placeService.getPlaceById(placeId));
+
+		for(Long userId : userIdList) {
+			User user = userService.getUserById(userId);
+			userRepository.delete(user);
+		}
 	}
 
 }
